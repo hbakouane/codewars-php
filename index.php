b237377637781c0d6f4396c8d997dafae6905a5d
= <=>
2021-03-27 11:22:51 +0100

    decode_morse

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 class Exercice
 {
-    public function spinWords($str)
+    public function spinWords($str) :string
     {
         $exploded = explode(' ', $str);
         foreach ($exploded as $key => $item) {
@@ -14,7 +14,20 @@ class Exercice
         }
         return implode(' ', $exploded);
     }
+
+    public function decode_morse(string $code) :string
+    {
+        $code = str_replace('.... ', 'H', $code);
+        $code = str_replace('-..', 'D', $code);
+        $code = str_replace('. ', 'E', $code);
+        $code = str_replace('-.--', 'Y', $code);
+        $code = str_replace('   ', ' ', $code);
+        $code = str_replace('.--- ', 'J', $code);
+        $code = str_replace('..- ', 'U', $code);
+        $code = str_replace(' .', 'E', $code);
+        return $code;
+    }
 }
 
 $instance = new Exercice();
-echo $instance->spinWords('Hey fellow warriors');
+echo $instance->decode_morse('.... . -.--  .--- ..- -.. .');
